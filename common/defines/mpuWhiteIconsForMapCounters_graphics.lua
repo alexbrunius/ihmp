c88d60e5185572001178949cb52dc59173806334
Jakub Mozgawa <jakub.mozgawa@gmail.com>
2020-04-07 21:48:46 +0200

    Remove friend/foe NATO counter coloring (so that NATO counter submod doesn't change the checksum)

---
+++ common/defines/mpuWhiteIconsForMapCounters_graphics.lua
@@ -0,0 +1,4 @@
+  -- unit on-map interface modulate colors
+  NDefines.NGraphics.FRIEND_COLOR  = {1.0, 1.0, 1.0}
+  NDefines.NGraphics.ENEMY_COLOR   = {1.0, 1.0, 1.0}
+  NDefines.NGraphics.NEUTRAL_COLOR = {1.0, 1.0, 1.0}
