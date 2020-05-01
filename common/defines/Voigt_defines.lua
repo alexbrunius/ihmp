35455f45716e7b48d99fd8734381a546bf993214
Voigit <39305136+Voigit@users.noreply.github.com>
2020-05-01 20:44:05 +0200

    Add files via upload

---
+++ common/defines/Voigt_defines.lua
@@ -0,0 +1,7 @@
+NDefines.NAI.MAX_AHEAD_RESEARCH_PENALTY = 12 --researching 4 years ahead of time
+
+-- for positive values of following defines, ai weights will take over of hardcoded ai scoring system
+NDefines.NAI.MIN_AI_SCORE_TO_MOBILIZATION_LAW_OVERRIDE_HARD_CODED_SCORE = 1.0
+NDefines.NAI.MIN_AI_SCORE_TO_ECONOMY_LAW_OVERRIDE_HARD_CODED_SCORE = 1.0
+NDefines.NAI.MIN_AI_SCORE_TO_TRADE_LAW_OVERRIDE_HARD_CODED_SCORE = 1.0
+NDefines.NAI.MIN_AI_SCORE_TO_ALL_LAWS_OVERRIDE_HARD_CODED_SCORE = 100.0
