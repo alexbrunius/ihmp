972cc452e5f1de6e7640aa7f88f6bfc62718ff03
Jakub Mozgawa <jakub.mozgawa@gmail.com>
2021-02-09 15:39:10 +0100

    [NC] update changelog script

--- changelog.sh
+++ changelog.sh
@@ -1,9 +1,9 @@
 if ! [ -x "$(command -v xclip)" ]; then
   touch new_changelog.txt
-  echo -e "$(git log --reverse --invert-grep --grep=\\[NC\\] --pretty="- %s." $1..HEAD)" > new_changelog.txt
+  echo -e "$(git log --reverse --invert-grep --grep=^\\[NC\\] --pretty="- %s." $1..HEAD)" > new_changelog.txt
   echo "Created new_changelog.txt"
 else
-  echo -e "$(git log --reverse --invert-grep --grep=\\[NC\\] --pretty="- %s." $1..HEAD)" | xclip -selection clipboard
+  echo -e "$(git log --reverse --invert-grep --grep=^\\[NC\\] --pretty="- %s." $1..HEAD)" | xclip -selection clipboard
   echo "Changelog copied to clipboard"
 fi
 todos map/definition.csv
